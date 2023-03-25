817696fb8a691bd60273244d65de4e10d90e9f24
kaustubh <kaustubhpardhi0062gmail.com>
2023-03-25 19:14:44 +0530

    feature: login screen

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,15 +1,38 @@
 import "./login.css";
+import user from "./user.jpg";
 function Login() {
   return (
     <div className="login-tab">
       <div className="login-card">
         <div className="login-1">
-          <p className="login-title">Welcome Back</p>
-          <input type="text"></input>
-          <input type="text"></input>
+          <p className="login-title">Welcome Back 👋</p>
+          <div className="input-item">
+            <p className="input-title">Email</p>
+            <input type="text" className="input-field"></input>
+          </div>
+          <div className="input-item">
+            <p className="input-title">Password</p>
+            <input type="text" className="input-field"></input>
+          </div>
+          <div className="login-button">
+            <button className="login-btn ">Continue 🡢 </button>
+          </div>
         </div>
+        <div className="vl"></div>
         <div className="login-2">
-          <p>testimonial</p>
+          <div className="login-testimonial">
+            <p className="testimonial">
+              "file.vault has helped us alot , we literally became a million
+              dollar company just after signing up with file.vault"
+            </p>
+          </div>
+          <div className="login-testimonial-user">
+            <img className="user-img" src={user}></img>
+            <span className="user-name">Priya Iyer</span>
+            <span className="user-designation">
+              Engineering Manager , Google
+            </span>
+          </div>
         </div>
       </div>
     </div>
